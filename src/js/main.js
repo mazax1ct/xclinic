9c914ae809265d3f85d3beae13cdf7a0b787f762
mazax1ct <vova@giveandget.ru>
2019-01-16 17:37:46 +0300

    press + spec

--- src/js/main.js
+++ src/js/main.js
@@ -209,30 +209,26 @@ $(document).ready(function() {
 
   $('[data-fancybox="gallery"]').fancybox({
     buttons: [
-      //"zoom",
-      //"share",
-      //"slideShow",
-      //"fullScreen",
-      //"download",
-      //"thumbs",
       "close"
     ],
     btnTpl: {
       close:
-      '<button data-fancybox-close class="fancybox-button fancybox-button--close" title="Закрыть">' +
-      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 10.6L6.6 5.2 5.2 6.6l5.4 5.4-5.4 5.4 1.4 1.4 5.4-5.4 5.4 5.4 1.4-1.4-5.4-5.4 5.4-5.4-1.4-1.4-5.4 5.4z"/></svg>' +
-      "</button>",
+        '<button data-fancybox-close class="fancybox-button fancybox-button--close" title="Закрыть">' +
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 10.6L6.6 5.2 5.2 6.6l5.4 5.4-5.4 5.4 1.4 1.4 5.4-5.4 5.4 5.4 1.4-1.4-5.4-5.4 5.4-5.4-1.4-1.4-5.4 5.4z"/></svg>' +
+        "</button>",
+      arrowLeft:
+        '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="Назад">' +
+        '<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.28 15.7l-1.34 1.37L5 12l4.94-5.07 1.34 1.38-2.68 2.72H19v1.94H8.6z"/></svg></div>' +
+        "</button>",
+      arrowRight:
+        '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="Вперёд">' +
+        '<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.4 12.97l-2.68 2.72 1.34 1.38L19 12l-4.94-5.07-1.34 1.38 2.68 2.72H5v1.94z"/></svg></div>' +
+        "</button>",
     }
   });
 
   $('[data-fancybox="video"]').fancybox({
     buttons: [
-      //"zoom",
-      //"share",
-      //"slideShow",
-      //"fullScreen",
-      //"download",
-      //"thumbs",
       "close"
     ],
     btnTpl: {
